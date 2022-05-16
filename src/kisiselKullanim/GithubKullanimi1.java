40e1da0e19f940951897313bb2ef3a3add2e3b97
Nevalyurtsever <94480387+Nevalyurtsever@users.noreply.github.com>
2022-05-16 14:00:19 +0300

    ilkdegisiklik

--- src/kisiselKullanim/GithubKullanimi1.java
+++ src/kisiselKullanim/GithubKullanimi1.java
@@ -3,5 +3,6 @@ package kisiselKullanim;
 public class GithubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("That's it");
     }
 }
