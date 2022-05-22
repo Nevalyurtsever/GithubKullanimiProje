1bfa2e6118522951e0715ad429e754133fcc48ba
Nevalyurtsever <94480387+Nevalyurtsever@users.noreply.github.com>
2022-05-22 20:55:20 +0300

    main branch degisiklik

--- src/kisiselKullanim/GithubKullanimi1.java
+++ src/kisiselKullanim/GithubKullanimi1.java
@@ -2,7 +2,13 @@ package kisiselKullanim;
 
 public class GithubKullanimi1 {
     public static void main(String[] args) {
+
         System.out.println("hello world");
+
         System.out.println("That's it");
+
+        System.out.println("main branch degisiklik 1. satır ");
+
+        System.out.println("main branch degisiklik 2.satir");
     }
 }
